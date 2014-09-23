e383467022e63cc7f1ce92ecb440f96eae934c2c
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-23 16:22:27 -0400

    adding options updates

--- public/js/settings_build.js
+++ public/js/settings_build.js
@@ -2,7 +2,64 @@
 'use strict';
 
 // var request = require('superagent');
-var updatestatus;
+var updatestatus,
+	themesettingsConfiguration,
+	themesettingsReadOnly,
+	appsettingsConfiguration,
+	appsettingsReadOnly;
+
+var jsonFormElements = function (options) {
+	var returnhtml = '',
+		jsonobject = options.jsonobject,
+		prependinputname = (options.prependinputname) ? options.prependinputname + '.' : '',
+		readonly = (options.readonly) ? 'disabled=disabled' : '',
+		idnameprepend = (options.idnameprepend) ? options.idnameprepend : 'jfe',
+		prependhtml = (options.prependhtml) ? options.prependhtml : '<div class="_pea-row _pea-container-forminput">',
+		appendhtml = (options.appendhtml) ? options.appendhtml : '</div>',
+		jreoptionvalues = options.jreoptionvalues;
+
+	for (var x in jsonobject) {
+		if (x.match('jfe-options-')) {
+			jreoptionvalues = jsonobject[x];
+			jreoptionvalues.name = x.replace('jfe-options-', '');
+		}
+		else if (typeof jsonobject[x] === 'object') {
+			returnhtml += jsonFormElements({
+				jsonobject: jsonobject[x],
+				prependinputname: x,
+				readonly: readonly,
+				idnameprepend: idnameprepend,
+				prependhtml: prependhtml,
+				appendhtml: appendhtml,
+				jreoptionvalues: jreoptionvalues
+			});
+		}
+		else {
+			var elementid = idnameprepend + '-' + prependinputname + x;
+			var elementname = prependinputname + x;
+			var elementval = jsonobject[x];
+			returnhtml += prependhtml;
+			returnhtml += '<label class="_pea-col-span3 _pea-label" for="' + elementid + '">' + elementname + '</label>';
+			if (jreoptionvalues && (jreoptionvalues.name === x) && jreoptionvalues.type === 'array' && jreoptionvalues.value) {
+				var selectOptionsFromDefaultVal = jreoptionvalues.value.split(',');
+				returnhtml += '<select class="_pea-col-span9 noFormSubmit" name="' + elementname + '">';
+				for (var j in selectOptionsFromDefaultVal) {
+					returnhtml += '<option ';
+					if (selectOptionsFromDefaultVal[j] === elementval) {
+						returnhtml += 'selected="selected"';
+					}
+					returnhtml += ' value="' + selectOptionsFromDefaultVal[j] + '">' + selectOptionsFromDefaultVal[j] + '</option>';
+				}
+				returnhtml += '</select>';
+			}
+			else {
+				returnhtml += '<input class="_pea-col-span9" type="text" id="' + elementid + '" name="' + elementname + '" value="' + elementval + '" ' + readonly + ' />';
+			}
+			returnhtml += appendhtml;
+		}
+	}
+	return returnhtml;
+};
 
 window.addEventListener('load', function () {
 	updatestatus = document.getElementById('update-status');
@@ -10,6 +67,29 @@ window.addEventListener('load', function () {
 		if (periodicversion.status === 'needupdate') {
 			updatestatus.style.display = 'block';
 		}
+	});
+
+	themesettingsConfiguration = document.getElementById('themesettings-config');
+	themesettingsReadOnly = document.getElementById('themesettings-readonly');
+	appsettingsConfiguration = document.getElementById('appsettings-config');
+	appsettingsReadOnly = document.getElementById('appsettings-readonly');
+	appsettingsConfiguration.innerHTML = jsonFormElements({
+		jsonobject: window.appsettings.configuration,
+		idnameprepend: 'asc'
+	});
+	appsettingsReadOnly.innerHTML = jsonFormElements({
+		jsonobject: window.appsettings.readonly,
+		readonly: true,
+		idnameprepend: 'asro'
+	});
+	themesettingsConfiguration.innerHTML = jsonFormElements({
+		jsonobject: window.themesettings.configuration,
+		idnameprepend: 'tsc'
+	});
+	themesettingsReadOnly.innerHTML = jsonFormElements({
+		jsonobject: window.themesettings.readonly,
+		readonly: true,
+		idnameprepend: 'tsro'
 	});
 });
 
