056db061169c3eb2640e8fa7257bcc60a634d1ca
yaw joseph etse <yaw.etse@gmail.com>
2017-07-10 07:57:18 -0400

    fixing auth route

--- routers/index.js
+++ routers/index.js
@@ -9,8 +9,8 @@ const extensionRouter = periodic.express.Router();
 const admin_route_prefix = adminSettings.routing.admin_prefix;
 const adminRoute = periodic.utilities.routing.route_prefix(admin_route_prefix);
 const passportControllers = periodic.controllers.extension.get('periodicjs.ext.passport');
-extensionRouter.use(passportControllers.auth.ensureAuthenticated);
 extensionRouter.use(controllers.admin.adminResLocals);
+extensionRouter.use(adminRoute, passportControllers.auth.ensureAuthenticated);
 extensionRouter.use(adminRoute, adminRouter);
 
 module.exports = extensionRouter;
