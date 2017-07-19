98e112fa487185729f54d4a787d6aed6eb1bdcb6
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-07-19 11:14:50 -0400

    Added support for file encryption

--- config/settings.js
+++ config/settings.js
@@ -1,10 +1,12 @@
 'use strict';
+const periodic = require('periodicjs');
 
 module.exports = {
   settings: {
     routing: {
       admin_prefix: 'b-admin',
     },
+    encryption_key_path: periodic.settings.application.server.https.ssl.private_key,
   },
   databases: {},
 };
--- routers/data.js
+++ routers/data.js
@@ -1,14 +1,28 @@
 'use strict';
 
+const fs = require('fs-extra');
 const periodic = require('periodicjs');
 const utilities = require('../utilities');
 const controllers = require('../controllers');
 const dataRouter = periodic.express.Router();
 const dataRouters = utilities.data.getDataCoreController();
-// const adminSettings = utilities.getSettings();
+const adminSettings = utilities.getSettings();
+const encryption_key = fs.readFileSync(adminSettings.encryption_key_path).toString();
+
+dataRouter.post('/standard_assets/encrypted_files', periodic.core.files.uploadMiddlewareHandler({
+  periodic,
+  encrypted_client_side: true, 
+  encryption_key,
+}));
+dataRouter.get('/standard_assets/decrypt_asset/:id/:filename', periodic.core.files.decryptAssetMiddlewareHandler({
+  periodic,
+  encryption_key,
+}));
+// console.log({ encryptionKey });
 Array.from(dataRouters.values()).forEach(drouter => {
   dataRouter.use(drouter.router);
 });
+
 // console.log(utilities.data.getDataCoreController());
 // dataRouter.get('/', controllers.extensions.getExtensions, controllers.extensions.extensionView);
 // dataRouter.get('/dashboard', controllers.admin.dashboardView);
