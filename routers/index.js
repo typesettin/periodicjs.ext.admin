5e299db46b9385044be0ed94942e87b7b8d2ab34
yaw joseph etse <yaw.etse@gmail.com>
2017-07-21 20:05:15 -0400

    Added support for user access control

--- routers/index.js
+++ routers/index.js
@@ -9,8 +9,12 @@ const extensionRouter = periodic.express.Router();
 const admin_route_prefix = adminSettings.routing.admin_prefix;
 const adminRoute = periodic.utilities.routing.route_prefix(admin_route_prefix);
 const passportControllers = periodic.controllers.extension.get('periodicjs.ext.passport');
+const uacControllers = periodic.controllers.extension.get('periodicjs.ext.user_access_control');
+const preTransforms = periodic.utilities.middleware.preTransforms(periodic);
+//	adminRouter.all('*', global.CoreCache.disableCache, authController.ensureAuthenticated, uacController.loadUserRoles, uacController.check_user_access);
+
 extensionRouter.use(controllers.admin.adminResLocals);
-extensionRouter.use(adminRoute, passportControllers.auth.ensureAuthenticated);
-extensionRouter.use(adminRoute, adminRouter);
+extensionRouter.use(adminRoute, passportControllers.auth.ensureAuthenticated, uacControllers.uac.loadUserRoles /*, uacControllers.uac.check_user_access - now handled as transforms*/ );
+extensionRouter.use(adminRoute, preTransforms, adminRouter);
 
 module.exports = extensionRouter;
